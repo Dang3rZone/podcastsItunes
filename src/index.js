835b7dcea3cea9fcff8eb88e0a8f878c0a7bdead
dang3rzone <andresrosalespinzon@gmail.com>
2023-02-23 19:53:50 +0100

    feat: cleran code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';
