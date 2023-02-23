2a14a9e754d37a70e4dcfe9ce105b8827941d081
dang3rzone <andresrosalespinzon@gmail.com>
2023-02-23 22:03:40 +0100

    feat: cleaned code, fixed repeated key error and episode list is done

--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -1,58 +1,92 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import {
-  ListGroup,
-  Spinner,
-  Row,
-  Col,
-  Card,
-  Table,
-  Container,
-} from 'react-bootstrap';
+import { Spinner, Row, Col, Card, Container } from 'react-bootstrap';
 import xml2js from 'xml2js';
 
+const ONE_DAY = 24 * 60 * 60 * 1000; // milliseconds in one day
+
 const EpisodesList = () => {
+  const [podcast, setPodcast] = useState({});
   const [episodes, setEpisodes] = useState([]);
   const [loading, setLoading] = useState(true);
   const { trackId } = useParams();
+  const episodesToShow = episodes.slice(0, 8);
 
   useEffect(() => {
     const fetchEpisodes = async () => {
-      setLoading(true);
+      try {
+        setLoading(true);
 
-      const response = await fetch(
-        `https://api.allorigins.win/get?url=${encodeURIComponent(
-          `https://itunes.apple.com/lookup?id=${trackId}`
-        )}`
-      );
-
-      const data = await response.json();
-      const prettyData = data.contents.replace(/\n/g, '');
-      const prettyToJSON = JSON.parse(prettyData);
-      const finalData = prettyToJSON.results[0].feedUrl;
-
-      const responseXML = await fetch(
-        `https://api.allorigins.win/get?url=${encodeURIComponent(
-          `${finalData}`
-        )}`
-      )
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
+        const cachedPodcast = localStorage.getItem(`podcast-${trackId}`);
+        const cachedEpisodes = localStorage.getItem(`episodes-${trackId}`);
+        if (cachedPodcast && cachedEpisodes) {
+          const parsedPodcast = JSON.parse(cachedPodcast);
+          const parsedEpisodes = JSON.parse(cachedEpisodes);
+          const ageInMs = Date.now() - parsedPodcast.timestamp;
+          if (ageInMs < ONE_DAY) {
+            setPodcast(parsedPodcast.data);
+            setEpisodes(parsedEpisodes.data);
+            setLoading(false);
+            return;
           }
-          throw new Error('Network response was not ok.');
-        })
-        .then((x) => {
-          //   console.log(x.contents);
-          const json = xml2js.parseStringPromise(x.contents);
+        }
 
-          console.log(json);
-          return json;
-        });
+        const response = await fetch(
+          `https://api.allorigins.win/get?url=${encodeURIComponent(
+            `https://itunes.apple.com/lookup?id=${trackId}`
+          )}`
+        );
+        if (!response.ok) {
+          throw new Error('Failed to fetch episodes');
+        }
 
-      console.log(responseXML.rss.channel);
-      setEpisodes(responseXML.rss.channel);
-      setLoading(false);
+        const data = await response.json();
+        const prettyData = data.contents.replace(/\n/g, '');
+        const prettyToJSON = JSON.parse(prettyData);
+        const podcastData = prettyToJSON.results;
+
+        setPodcast(podcastData[0]);
+        console.log('podcast', podcastData);
+        const finalData = prettyToJSON.results[0].feedUrl;
+
+        const responseXML = await fetch(
+          `https://api.allorigins.win/get?url=${encodeURIComponent(
+            `${finalData}`
+          )}`
+        )
+          .then((response) => {
+            if (response.ok) {
+              return response.json();
+            }
+            throw new Error('Network response was not ok.');
+          })
+          .then((x) => {
+            if (x && x.contents) {
+              // check if x exists and has a contents property
+              const json = xml2js.parseStringPromise(x.contents);
+
+              return json;
+            } else {
+              throw new Error('XML response was invalid.');
+            }
+          });
+
+        console.log('episodes', responseXML.rss.channel[0].item);
+
+        // localStorage.setItem(
+        //   `episodes-${trackId}`,
+        //   JSON.stringify({
+        //     timestamp: Date.now(),
+        //     data: responseXML.rss.channel[0].item,
+        //   })
+        // );
+
+        setEpisodes(responseXML.rss.channel[0].item);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      }
     };
 
     fetchEpisodes();
@@ -61,26 +95,51 @@ const EpisodesList = () => {
   return (
     <>
       {loading ? (
-        <Spinner animation="border" />
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Fetching episodes...</span>
+        </Spinner>
       ) : (
-        <div className="container">
-          {episodes.map((episode) => (
-            <Card key={episode.id} style={{ width: '18rem' }}>
-              <Card.Img
-                variant="top"
-                src={episode.image[0].url}
-                key={episode.id}
-              />
-              <Card.Body>
-                <Card.Title>{episode.title}</Card.Title>
-                <Card.Text>{episode.description}</Card.Text>
-                <Card.Link href={episode.link}>Listen Now</Card.Link>
-              </Card.Body>
-            </Card>
-          ))}
-        </div>
+        <Container>
+          <Row>
+            <Col md={4}>
+              <Card style={{ width: '100%' }}>
+                <Card.Img variant="top" src={podcast.artworkUrl600} />
+                <Card.Body>
+                  <Card.Title>{podcast.trackName}</Card.Title>
+                  <Card.Text>{podcast.artistName}</Card.Text>
+                  <Card.Text>{podcast.collectionName}</Card.Text>
+                  <Card.Text>{podcast.primaryGenreName}</Card.Text>
+                  <Card.Text>{podcast.releaseDate}</Card.Text>
+                  <Card.Link href={podcast.collectionViewUrl}>
+                    View on iTunes
+                  </Card.Link>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={8}>
+              {episodesToShow.map((episode, index) => (
+                <Card key={index}>
+                  <Card.Body>
+                    <Card.Title>{episode.title}</Card.Title>
+                    <Card.Text>
+                      {episode.description[0] &&
+                      episode.description[0].replace(/(<([^>]+)>)/gi, '')
+                        .length > 250
+                        ? `${episode.description[0]
+                            .replace(/(<([^>]+)>)/gi, '')
+                            .slice(0, 250)}...`
+                        : episode.description[0].replace(/(<([^>]+)>)/gi, '')}
+                    </Card.Text>
+                    <Card.Link href={episode.link}>Listen Now</Card.Link>
+                  </Card.Body>
+                </Card>
+              ))}
+            </Col>
+          </Row>
+        </Container>
       )}
     </>
   );
 };
+
 export default EpisodesList;
