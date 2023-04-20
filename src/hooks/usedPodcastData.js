0713518dcdb4d2c5237476de759c7d466bde4a39
dang3rzone <andresrosalespinzon@gmail.com>
2023-04-20 13:35:56 +0200

    refactor: better redeability and usememo

--- src/hooks/usedPodcastData.js
+++ src/hooks/usedPodcastData.js
@@ -1,25 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 export const usePodcastData = () => {
   const [podcasts, setPodcasts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const podcastListUrl =
+  const iTunesPodcastSearchUrl =
     'https://itunes.apple.com/search?term=podcast&country=US&limit=100';
 
-  useEffect(() => {
-    const fetchPodcasts = async () => {
-      const response = await fetch(
-        `https://api.allorigins.win/get?url=${encodeURIComponent(
-          `${podcastListUrl}`
-        )}`
-      );
-      const data = await response.json();
-      const prettyToJSON = JSON.parse(data.contents);
-      await setPodcasts(prettyToJSON.results);
-      setIsLoading(false);
-    };
-    fetchPodcasts();
+  const fetchPodcasts = useCallback(async () => {
+    const response = await fetch(
+      `https://api.allorigins.win/get?url=${encodeURIComponent(
+        `${iTunesPodcastSearchUrl}`
+      )}`
+    );
+    const data = await response.json();
+    const prettyToJSON = JSON.parse(data.contents);
+    await setPodcasts(prettyToJSON.results);
+    setIsLoading(false);
   }, []);
+
+  useEffect(() => {
+    fetchPodcasts();
+  }, [fetchPodcasts]);
 
   return {
     podcasts,
